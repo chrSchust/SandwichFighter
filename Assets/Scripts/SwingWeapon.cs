274d32734fbb86c8e878bf7698c88d70087b855b
chrSchust <chr.schust@gmail.com>
2016-10-26 13:11:32 +0200

    Asset folder fix, hit detection only on mouse button pressed

---
+++ Assets/Scripts/SwingWeapon.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingWeapon : MonoBehaviour {
+
+    public float speed = 0.1F;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, new Quaternion(0.3f, -0.2f, 0.1f, 0.9f), Time.deltaTime * speed);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, new Quaternion(0f, 0f, 0f, 1f), Time.deltaTime * speed);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(col.gameObject.name);
+
+        if (col.gameObject.tag == "Enemy" && Input.GetMouseButton(0))
+        {
+            Debug.Log("hit");
+            col.gameObject.GetComponent<MoveToPlayer>().HitByPlayer(10);
+        }
+    }
+}
