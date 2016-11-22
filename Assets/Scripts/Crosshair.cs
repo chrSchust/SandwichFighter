7bdd38d66b2e5ac6a708447e740d0588e32e5dad
Kevin Klugmann <kevin.klugmann@web.de>
2016-11-22 17:16:39 +0100

    set crosshair dot

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+	public float crosshairSizeMultiplicator = 0.1f;
+	public Texture crosshairTexture;
+	Rect crosshairRect;
+	// Use this for initialization
+	void Start () {
+		float crosshairSize = Screen.width * crosshairSizeMultiplicator;
+		//crosshairTexture = Resources.Load ("Textures/crosshair") as Texture;
+		crosshairRect = new Rect (Screen.width / 2 - crosshairSize / 2, 
+			Screen.height / 2 - crosshairSize / 2,
+			crosshairSize,
+			crosshairSize);
+	}
+
+	void OnGUI() {
+		GUI.DrawTexture (crosshairRect, crosshairTexture);
+	}
+}
