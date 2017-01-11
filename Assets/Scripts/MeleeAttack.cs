3b91e5f26e33a3abc1f42eb7043d0e3a0374495f
chrSchust <chr.schust@gmail.com>
2017-01-11 16:21:51 +0100

    Only reduce bread health on hit

--- Assets/Scripts/MeleeAttack.cs
+++ Assets/Scripts/MeleeAttack.cs
@@ -129,63 +129,62 @@ public class MeleeAttack : MonoBehaviour
             }
         }
 
+        if (breadHealth1 != null && breadHealth1 <= 0 && breadHealth2 != null && breadHealth2 <= 0)
+        {
+            Debug.Log("both dead");
+            weapon.GetComponentInChildren<Renderer>().enabled = false;
+            return;
+        }
+        else { weapon.GetComponentInChildren<Renderer>().enabled = true; }
+
         if (Input.GetButtonDown("Fire1"))
         {
-            if (activeIngredients != null)
-            {
-                foreach (Ingredient i in activeIngredients)
-                {
-                    //Debug.Log(i.getName());
-                }
-            }
-
-            if (breadHealth1 != null && breadHealth1 <= 0 && breadHealth2 != null && breadHealth2 <= 0)
-            {
-                Debug.Log("both dead");
-                weapon.GetComponentInChildren<Renderer>().enabled = false;
-                return;
-            }
-            else { weapon.GetComponentInChildren<Renderer>().enabled = true; }
-
-
-            if (activebread != null && activebread == bread1)
-            {
-                breadHealth1--;
-				guiManager.SetBread1Hits (breadHealth1);
-                if (breadHealth1 <= 0)
-                {
-                    Debug.Log("bread 1 dead");
-                    if (breadHealth2 > 0)
-                    {
-                        activeIngredients = ingredients2;
-                        activebread = bread2;
-                        weapon.GetComponentInChildren<Renderer>().material = material2;
-                    }
-
-                }
-            }
-            if (activebread != null && activebread == bread2)
-            {
-                breadHealth2--;
-				guiManager.SetBread2Hits (breadHealth2);
-                if (breadHealth2 <= 0)
-                {
-                    Debug.Log("bread 2 dead");
-                    if (breadHealth1 > 0)
-                    {
-                        activeIngredients = ingredients1;
-                        activebread = bread1;
-                        weapon.GetComponentInChildren<Renderer>().material = material1;
-                    }
-                }
-            }
-
             RaycastHit[] allHits;
             allHits = Physics.SphereCastAll(transform.position, hitRadius, transform.forward, maxHitDistance, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Ignore);
             foreach (RaycastHit hit in allHits)
             {
                 if (hit.transform.gameObject.tag == "Enemy")
                 {
+                    if (activeIngredients != null)
+                    {
+                        foreach (Ingredient i in activeIngredients)
+                        {
+                            //Debug.Log(i.getName());
+                        }
+                    }
+
+                    if (activebread != null && activebread == bread1)
+                    {
+                        breadHealth1--;
+                        guiManager.SetBread1Hits(breadHealth1);
+                        if (breadHealth1 <= 0)
+                        {
+                            Debug.Log("bread 1 dead");
+                            if (breadHealth2 > 0)
+                            {
+                                activeIngredients = ingredients2;
+                                activebread = bread2;
+                                weapon.GetComponentInChildren<Renderer>().material = material2;
+                            }
+
+                        }
+                    }
+                    if (activebread != null && activebread == bread2)
+                    {
+                        breadHealth2--;
+                        guiManager.SetBread2Hits(breadHealth2);
+                        if (breadHealth2 <= 0)
+                        {
+                            Debug.Log("bread 2 dead");
+                            if (breadHealth1 > 0)
+                            {
+                                activeIngredients = ingredients1;
+                                activebread = bread1;
+                                weapon.GetComponentInChildren<Renderer>().material = material1;
+                            }
+                        }
+                    }
+
                     hit.transform.gameObject.GetComponent<EnemyController>().HitByPlayer(activeIngredients, activebread);
                     break;
                 }
